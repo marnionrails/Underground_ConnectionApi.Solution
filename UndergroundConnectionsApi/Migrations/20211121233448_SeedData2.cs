069ef529d67281c74114f55167e834807f09c29d
Sisi Vieira <cicy886@gmail.com>
2021-11-29 18:28:07 -0800

    new Classification seed data migration

---
+++ UndergroundConnectionsApi/Migrations/20211121233448_SeedData2.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace UndergroundConnectionsApi.Migrations
+{
+    public partial class SeedData2 : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.InsertData(
+                table: "Classifications",
+                columns: new[] { "ClassificationId", "ClassificationName", "ClassificationSpecification" },
+                values: new object[] { 1, "Singer", "Country" });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DeleteData(
+                table: "Classifications",
+                keyColumn: "ClassificationId",
+                keyValue: 1);
+        }
+    }
+}
